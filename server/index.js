4ad79934e9ab692444af0f761bf5d5ea99e91bc0
Drishya Ajith <drishyaajith10@gmail.com>
2026-02-28 05:02:11 +0530

    IDE done

--- server/index.js
+++ server/index.js
@@ -44,6 +44,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(8000, () => {
-  console.log("Server running on port 8000");
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
